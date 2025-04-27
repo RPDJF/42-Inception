a9589e70ab2a0305dee1ce19d873f89a55f5c114
RPDJF <ruipaulo.unify@outlook.fr>
2025-04-27 19:20:35 +0200

    Add qBittorrent service configuration with default settings and volume mappings

--- srcs/requirements/qbittorrent/docker-entrypoint.sh
+++ srcs/requirements/qbittorrent/docker-entrypoint.sh
@@ -1,1 +1,20 @@
+if [ ! -d "$HOME/.config/qBittorrent/qBittorrent.conf" ]; then
+	echo Setting default config...
+	mkdir -p ~/.config/qBittorrent
+	mkdir -p /var/logs/qBittorrent
+	echo Changing creds...
+	PASSWORD_HASH=$(echo -n "${QBITTORRENT_PASSWORD}" | sha1sum | awk '{print $1}')
+	cat <<EOF > ~/.config/qBittorrent/qBittorrent.conf
+[Application]
+FileLogger\Path=/dev/stdout
+
+[BitTorrent]
+Session\DefaultSavePath=/Downloads
+
+[Preferences]
+WebUI\Password_PBKDF2="@ByteArray(Dmg9acSaXf9jhhNOihXWjg==:yBX9oYePcRSipGUAtDeM38+28erEY4rZMpKqor8Gr99lGWPgAznXPSoGHdV7/PcWqzSDfGcK04kaxLpWRIwp+g==)"
+
+EOF
+fi
+
 qbittorrent-nox
