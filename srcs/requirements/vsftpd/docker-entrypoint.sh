b90adde12f5d284365078c244849a68c3449d53e
RPDJF <ruipaulo.unify@outlook.fr>
2025-04-27 22:58:30 +0200

    Fix ownership of vsftpd configuration file before execution

--- srcs/requirements/vsftpd/docker-entrypoint.sh
+++ srcs/requirements/vsftpd/docker-entrypoint.sh
@@ -3,4 +3,6 @@
 adduser -D -g "$FTP_USER" -h /var/www/wordpress $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
+chown root:root /etc/vsftpd/vsftpd.conf
+
 exec vsftpd /etc/vsftpd/vsftpd.conf
